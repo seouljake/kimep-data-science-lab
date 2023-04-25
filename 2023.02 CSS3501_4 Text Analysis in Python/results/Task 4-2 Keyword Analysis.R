ed1d475a125a13d5848e94765dc72e679994cca5
seouljake <seouljake@gmail.com>
2023-04-25 16:18:16 +0600

    updates

--- 2023.02 CSS3501_4 Text Analysis in Python/results/Task 4-2 Keyword Analysis.R
+++ 2023.02 CSS3501_4 Text Analysis in Python/results/Task 4-2 Keyword Analysis.R
@@ -22,19 +22,31 @@ ts$Keyword <- ordered(ts$Keyword,levels=unique(ts$Keyword))
 
 # Plot Top 25 Keyword Frequencies
 p<-ggplot(dat=v1,aes(x=keywords,y=counts))+
-  geom_bar(stat="identity")
+  geom_bar(stat="identity")+
+  scale_x_discrete(guide = guide_axis(angle = 45)) +
+  ggtitle("Jacob's Top 25 Keyword Frequencies")
 p
+
+ggsave("top25.png",plot = p,height=110,width=200,units="mm",dpi = 500)
 
 
 # Plot Top 25 Keyword Frequencies, two Periods
 p<-ggplot(dat=v2,aes(x=Keyword,y=Frequency,fill=Period))+
   geom_bar(stat="identity",position=position_dodge())+
-  ggtitle("Sanat's Top 25 Keyword Frequencies")
+  scale_x_discrete(guide = guide_axis(angle = 45)) +
+  ggtitle("Jacob's Top 25 Keyword Frequencies, Two Periods")
 p
+
+ggsave("top25_2periods.png",plot = p,height=110,width=200,units="mm",dpi = 500)
 
 
 # Keyword Time-Series
 p<-ggplot(dat=ts,aes(x=Year,y=Frequency,group=Keyword))+
   geom_line(aes(color=Keyword))+
-  geom_point(aes(color=Keyword))
+  geom_point(aes(color=Keyword))+
+  ggtitle("Jacob's Keyword Time Series for Selected Keywords")
 p
+
+ggsave("keyword timeseries.png",plot = p,height=110,width=200,units="mm",dpi = 500)
+
+
